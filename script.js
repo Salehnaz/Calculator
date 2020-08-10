fb15e3a0a7a0b94b999ec75df01c4f0f4d76b96a
Salehnaz <65551578+Salehnaz@users.noreply.github.com>
2020-08-10 17:17:32 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function buttonclick(val)
+{
+    document.getElementById("screen").value=document.getElementById("screen").value+val;
+}
+
+
+function cleardisplay()
+{
+
+    document.getElementById("screen").value=""
+
+}
+
+function equalClick(){
+
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
